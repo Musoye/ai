cf031f0b1f9fe01b3df387703e4f3ee4b641cf96
musoye <oyebamijimustapha44@gmail.com>
2025-01-05 09:29:39 +0100

    feat: retrained model for better accuracy

--- facial_emotion/load_model.py
+++ facial_emotion/load_model.py
@@ -1,32 +1,47 @@
-import torch.nn as nn
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
 
+
+class EmotionClassification(nn.Module):
+    def __init__(self, num_classes=7):
+        super(EmotionClassification, self).__init__()
+        
+        # Feature extraction
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1) 
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) 
+
+        # Classification
+        self.fc1 = nn.Linear(128 * 6 * 6, 256) 
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        # Featurizer
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+
+        # Classifier
+        x = torch.flatten(x, start_dim=1) 
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x) 
+
+        return x
+
+
 def predict_image(filename):
-    model = nn.Sequential()
-
-    #featurizer
-    n_ch = 1
-    model.add_module('c1', nn.Conv2d(
-        in_channels=1, out_channels=n_ch, kernel_size=3
-    ))
-    model.add_module('relu1', nn.ReLU())
-    model.add_module('maxp1', nn.MaxPool2d(kernel_size=2))
-    model.add_module('flatten', nn.Flatten())
-
-    #classfication
-    model.add_module('fc1', nn.Linear(n_ch*13*13, 70))
-    model.add_module('relu2', nn.ReLU())
-    model.add_module('fc3', nn.Linear(70, 35))
-    model.add_module('relu3', nn.ReLU())
-    model.add_module('fc4', nn.Linear(35, 12))
-    model.add_module('relu4', nn.ReLU())
-    model.add_module('fc2', nn.Linear(12, 7))
+    model = EmotionClassification(num_classes=7)
 
     transform = transforms.Compose([
     transforms.Grayscale(),
-    transforms.Resize((28, 28)),
+    transforms.Resize((48, 48)),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [0.5])
 ])
